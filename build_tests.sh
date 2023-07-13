c45a6f7de69bbbb2346537ca03ce1445104cade3
Tom Finet <tom.codeninja@gmail.com>
2023-07-13 20:37:09 +0100

    script to build kernel for unit testing

--- build_tests.sh
+++ build_tests.sh
@@ -9,3 +9,12 @@ mkdir -p "$DIR/build" && cd "$DIR/build"
 cmake .. -DCMAKE_TOOLCHAIN_FILE=toolchain/CMakeToolchain.txt -DKTESTS=True
 make preprocess_linker_script
 make install
+
+# make returns 2 if errors
+if make --directory="."
+then
+    make image 
+    make qemu
+else
+    exit 1
+fi
