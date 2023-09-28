4a78f1216210e8759bfc47d9f3e3620d155616bf
Tom Finet <tom.codeninja@gmail.com>
2023-09-28 07:46:04 +0100

    implemented kmalloc with a test.

--- include/memory/vmm.h
+++ include/memory/vmm.h
@@ -1,54 +1,11 @@
-/*
-
-MIT License
-
-Copyright (c) 2023 Tom Finet
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-*/
-
 #pragma once
 
 #include <stddef.h>
 
-#include <list.h>
-#include <memory/paging.h>
+#include <memory/kslab.h>
 
-/* Kernel's virtual memory manager, implementing a crappy kmalloc. */
+/* Allocate nbytes. */
+void* kmalloc(size_t nbytes);
 
-// TODO: optimise virtual memory allocation for networking.
-
-// Descrribes a free contiguous virtual memory area
-// (vma), embedded in memory at the start of each vma.
-// size refers to the number of bytes in the vma
-// excluding this header.
-struct free_vma_t {
-	uint32_t size;
-	struct list_head list;
-};
-
-/* Dynamically allocated kernel virtual memory is managed starting from here. */
-extern va_t _kheap_start __attribute__((aligned(PAGE_NBYTES)));
-
-void vmm_init();
-
-/// Allocate a contiguous block of memory
-/// in kernel space of the specified size.
-va_t vmm_alloc_kheap(uint32_t size);
+/* Free the object */
+int kfree(void *objp);
---
+++ kernel/tests/memory/vmm_tests.c
@@ -0,0 +1,37 @@
+#include <tests/ktest.h>
+#include <memory/mm.h>
+#include <memory/paging.h>
+#include <memory/kheap.h>
+#include <memory/kslab.h>
+#include <memory/vmm.h>
+
+
+static void suite_init(void) {
+  mm_init();
+  page_init();
+  kheap_init(); 
+  kcache_init();
+}
+
+static void suite_exit(void) { }
+
+static void test_kmalloc() {
+  void *mem = kmalloc(4);
+  struct kslab_t *slab = page_descriptor(mem)->slab;
+  struct kcache_t *cache = list_entry(slab->list.next, struct kcache_t, slab_partial);
+  ASSERT(cache->objsize == 4);
+}
+
+static struct ktest_case_t vmm_cases[] = {
+  KTEST(test_kmalloc),
+};
+
+static struct ktest_suite_t vmm_suite = {
+  .name = "vmm_suite",
+  .init = suite_init,
+  .exit = suite_exit,
+  .cases = vmm_cases,
+  .num_cases = (sizeof(vmm_cases) / sizeof(struct ktest_case_t)),
+};
+
+KTEST_SUITE(vmm_suite);
