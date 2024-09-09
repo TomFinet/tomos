c9039ce12451c67b409354177493b3d0b1e0c30d
Tom Finet <tomfinet@outlook.com>
2024-09-09 08:12:50 +0100

    kernel: include: Move macros to kbit.h

--- include/klib/kbitmap.h
+++ include/klib/kbitmap.h
@@ -3,36 +3,32 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define NOT_FOUND -1
-#define ALL_SET 0xffffffff
-
+#define NOT_FOUND   -1
+#define ALL_SET     0xffffffff
 #define BITS_IN_BLK 32
 
-#define BIT(n) (1 << (n))
-#define BITMASK(n) (BIT(n) - 1)
-
-#define BITMAP_BLKS(n) ((n + BITS_IN_BLK - 1) / BITS_IN_BLK)
+#define BITMAP_BLKS(n)    ((n + BITS_IN_BLK - 1) / BITS_IN_BLK)
 #define BITMAP(bitmap, n) uint32_t bitmap[BITMAP_BLKS(n)]
 
 #define BITMAP_BLK(n) ((n) / BITS_IN_BLK)
 #define BITMAP_POS(n) ((n) % BITS_IN_BLK)
 
-static inline bool bitmap_read(uint32_t* bitmap, int n)
+static inline bool bitmap_read(uint32_t *bitmap, int n)
 {
 	return bitmap[BITMAP_BLK(n)] & (1 << BITMAP_POS(n));
 }
 
-static inline void bitmap_set(uint32_t* bitmap, int n)
+static inline void bitmap_set(uint32_t *bitmap, int n)
 {
 	bitmap[BITMAP_BLK(n)] = bitmap[BITMAP_BLK(n)] | (1 << BITMAP_POS(n));
 }
 
-static inline void bitmap_clear(uint32_t* bitmap, int n)
+static inline void bitmap_clear(uint32_t *bitmap, int n)
 {
 	bitmap[BITMAP_BLK(n)] = bitmap[BITMAP_BLK(n)] & ~(1 << BITMAP_POS(n));
 }
 
-static inline int bitmap_first_clear(uint32_t* bitmap, int bits_in_bitmap)
+static inline int bitmap_first_clear(uint32_t *bitmap, int bits_in_bitmap)
 {
 	int i = 0;
 	int size = BITMAP_BLKS(bits_in_bitmap);
--- include/memory/paging.h
+++ include/memory/paging.h
@@ -4,38 +4,34 @@
 
 #include <memory/frame.h>
 #include <memory/address.h>
+
+#include <klib/kbit.h>
 #include <panic.h>
 
-#define PAGE_NBYTES BIT(PAGE_ORDER)
-#define PTE_COUNT BIT(PTE_ORDER)
-#define PDE_COUNT BIT(PDE_ORDER)
-#define PAGE_COUNT BIT(PDE_ORDER + PTE_ORDER)
-
 /* Page flags */
-#define PAGE_PRESENT(x) (x)
-#define PAGE_RW(x)      ((x) << 0x01)
-#define PAGE_US(x)      ((x) << 0x02)
-#define PAGE_PWT(x)     ((x) << 0x03)
-#define PAGE_PCD(x)     ((x) << 0x04)
-#define PAGE_A(x)       ((x) << 0x05)
-#define PAGE_D(x)       ((x) << 0x06)
-#define PAGE_PAT(x)     ((x) << 0x07)
-#define PAGE_G(x)       ((x) << 0x08)
+#define PAGE_PRESENT(x) BITSET(x, 0)
+#define PAGE_RW(x)      BITSET(x, 1)
+#define PAGE_US(x)      BITSET(x, 2)
+#define PAGE_PWT(x)     BITSET(x, 3)
+#define PAGE_PCD(x)     BITSET(x, 4)
+#define PAGE_A(x)       BITSET(x, 5)
+#define PAGE_D(x)       BITSET(x, 6)
+#define PAGE_PAT(x)     BITSET(x, 7)
+#define PAGE_G(x)       BITSET(x, 8)
 
 #define PAGE_STD(pa) ((pa) | PAGE_PRESENT(1) | PAGE_RW(1))
 
-#define PDE_IDX(va) ((va) >> (PTE_ORDER + PAGE_ORDER))
-#define PTE_IDX(va) (((va) >> PAGE_ORDER) & BITMASK(PTE_ORDER))
+#define PAGE_IDX(va) ((va) >> PAGE_ORDER)
+#define PDE_IDX(va)  (PAGE_IDX(va) >> PTE_ORDER)
+#define PTE_IDX(va)  (PAGE_IDX(va) & BITMASK(PTE_ORDER))
 
 /* Page flag checks */
 #define PAGE_PA(pte)    ((pte) & ~0xfff)
 #define IS_PRESENT(pte) ((pte) & 0x1)
 #define IS_RW(pte)      ((pte) & 0x2)
 
-#define PAGE_FREE_SUCCESS  1
-#define PAGE_INVALID_BASE  -1
-#define PAGE_ALREADY_FREE  -1
-#define PAGE_IN_USE        -2
+#define PAGE_FREE_SUCCESS 1
+#define PAGE_ALREADY_FREE -1
 
 /* page table entry */
 typedef uint32_t pte_t;
@@ -43,7 +39,7 @@ typedef uint32_t pte_t;
 /* page directory entry */
 typedef uint32_t pde_t;
 
-void* page_alloc();
+void *page_alloc();
 int page_free(va_t vbase);
-struct frame_t* page_descriptor(void *va);
+struct frame_t *page_descriptor(void *va);
 void page_init(void);
