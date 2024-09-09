f40c8aeeb2d98aa1c1de9fb433b0d5f71d784bb6
Tom Finet <tomfinet@outlook.com>
2024-09-09 08:12:50 +0100

    kernel: include: Move macros to kbit.h

--- include/klib/kbit.h
+++ include/klib/kbit.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#define BITSET(b, n)	((b) << (n))
-#define BIT(n)		BITSET(1, n)
-#define BITGET(x, n)	((x) & BIT(n))
-#define BITMASK(n)	(BIT(n) - 1)
+#define BITSET(b, n) ((b) << (n))
+#define BIT(n)       BITSET(1, n)
+#define BITGET(x, n) ((x) & BIT(n))
+#define BITMASK(n)   (BIT(n) - 1)
+
+#define KiB(n) ((n) << 10)
+#define MiB(n) ((n) << 20)
