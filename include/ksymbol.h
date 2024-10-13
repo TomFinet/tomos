46f429c4b856e3d05bd60628fcafcfcc0842f5d4
Tom Finet <tomfinet@outlook.com>
2024-10-13 17:13:07 +0100

    ksymbol: Add macro for reading the symbols with physical addresses straight into global variables with virtual memory address

--- include/ksymbol.h
+++ include/ksymbol.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <memory/memory_def.h>
+
 #define SYMBOL_DEFINE(symbol, type)                                       \
 	extern void *_##symbol;                                           \
 	static type symbol = ((type)&_##symbol)
+
+#define SYMBOL_PA_DEFINE(symbol, type)	\
+	extern void *_##symbol;		\
+	static type symbol = ((type)(((pa_t)(&_##symbol)) + KERNEL_VA_BASE))
 
 #define SYMBOL_READ(symbol, type) ((type)&symbol)
