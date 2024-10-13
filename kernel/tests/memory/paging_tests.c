3439bd523a57cd8748b5c1181ab9c949c1380518
Tom Finet <tomfinet@outlook.com>
2024-10-13 17:16:16 +0100

    kernel: tests: Bulk page alloc and free test for linear allocation now checks that for every allocated page, the virtual address is KERNEL_VA_BASE larger than its physical address.

--- kernel/tests/memory/paging_tests.c
+++ kernel/tests/memory/paging_tests.c
@@ -4,6 +4,7 @@
 #include <memory/zone.h>
 #include <memory/paging.h>
 #include <memory/page_alloc.h>
+#include <memory/page_table.h>
 #include <tests/ktest.h>
 
 SYMBOL_DEFINE(kernel_va_end, va_t);
@@ -17,14 +18,6 @@ static void suite_exit(void)
 {
 }
 
-/**
- * Startup.s sets up the page directory and 3 page tables.
- * One is an identity mapped page table, i.e. virtual address is physical
- * address. The other two correspond to the upper quarter kernel, 2048 pages are
- * mapped starting from 0xc0000000, thus ending at 0xc0400000 At startup, we
- * only want to allocate enough for the kernel text and data. The rest can be
- * done on demand.
- */
 void test_paging_alloc_and_free(void)
 {
 	pg_idx_t expected_pg_idx = PAGE_IDX(kernel_va_end);
@@ -33,20 +26,35 @@ void test_paging_alloc_and_free(void)
 	ASSERT(free_linear(pg_idx) == pg_idx);
 }
 
+/*
+ * We can fit 1024 page entries in a page table
+ * We want to fill the page table, so allocate 1025 pages to guarantee this
+ * Check that the allocations are linear: va = pa + OFFSET
+ */
 void test_paging_bulk_alloc_and_free(void)
 {
-#define n 10
+#define n 2048
 	pg_idx_t pages[n];
 	pages[0] = alloc_linear();
 	ASSERT(pages[0] >= PAGE_IDX(kernel_va_end));
 
 	for (int i = 1; i < n; i++) {
 		pages[i] = alloc_linear();
-		ASSERT(pages[i] == pages[i - 1] + 1);
+
+		pde_t pg_pde = read_pde_for_pg(pages[i]);
+		pte_t pg_pte = read_pte(pg_pde, pages[i]);
+		va_t pg_va = PAGE_VA(pages[i]);
+		pa_t pg_pa = PAGE_PA(pg_pte);
+
+		ASSERT(pg_va == pg_pa + ZONE_LINEAR_VA_BASE);
 	}
 
 	for (int i = 0; i < n; i++) {
 		free_linear(pages[i]);
+		pde_t pg_pde = read_pde_for_pg(pages[i]);
+		pte_t pg_pte = read_pte(pg_pde, pages[i]);
+
+		ASSERT(!IS_PRESENT(pg_pte));
 	}
 }
 
