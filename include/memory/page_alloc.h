4388a9207976ddf4b0b9a0f2725574fabb1140c8
Tom Finet <tomfinet@outlook.com>
2024-10-13 17:26:54 +0100

    kernel: memory: page_alloc: Page allocation when we need a new page table to map the requested page now works.

--- include/memory/page_alloc.h
+++ include/memory/page_alloc.h
@@ -5,9 +5,13 @@
 #include <memory/paging.h>
 
 /* Allocate a single page in the linear address zone. */
-va_t alloc_linear(void);
+pg_idx_t alloc_linear(void);
 pg_idx_t free_linear(pg_idx_t pg_to_free);
+
+pg_idx_t alloc_pg_table(pg_idx_t pg_idx);
 
 va_t alloc_adhoc(void);
 
 frame_t *pg_linear_descriptor(va_t pg_va);
+
+void zero_pg(pg_idx_t pg_idx);
